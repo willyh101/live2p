f607a66b3f20ff6f4acee2e7b5b11d247ba795f8
will <hendricksw@gmail.com>
2021-05-17 15:00:58 -0700

    update process_data and fix stim alignment

--- live2p/analysis/generic.py
+++ live2p/analysis/generic.py
@@ -4,122 +4,69 @@ import warnings
 
 import numpy as np
 import scipy.stats as stats
-import sklearn
-
-from ..utils import load_json
+import sklearn.preprocessing
+import pandas as pd
 
 with warnings.catch_warnings():
     warnings.simplefilter('ignore', category=FutureWarning)
     import caiman as cm
     
-def process_data(c, splits, fr, stim_times=None, normalizer='scale', align_to=1, total_length=4):
+def process_data(raw_traces, trial_lengths, fr, new_start, norm_method='scale', stim_times=None, total_length=None):
     
-    traces, trialwise_data = clean_data(c, splits, normalizer=normalizer)
     
-    # convert seconds to frames
-    align_to *= int(fr)
-    total_length *= int(fr)
-    baseline_length = align_to - 1
-    
-    if stim_times is not None:
-        stim_times *= int(fr)
-        trialwise_data = do_stimalign(trialwise_data, stim_times, align_to)
-        
-    trialwise_data = baseline_subtract(trialwise_data, baseline_length)
-    psths = cut_psths(trialwise_data, length=total_length)
-    
-    return traces, psths
-    
-def clean_data(c, trial_lengths, normalizer='scale'):
-    """Min subtract, normalize, and make trialwise."""
-    
-    data  =  np.asarray(c)
     # min subtract and normalize
+    data  =  np.array(raw_traces)
     data = min_subtract(data)
     
-    norm_routines = {
-        'none': data, # nothing done...
-        'minmax': sklearn.preprocessing.minmax_scale(data, axis=1), # scaled to min max (not abs)
-        'zscore': stats.zscore(data, axis=1), # old fashion zscoring
-        'norm': sklearn.preprocessing.normalize(data, axis=1), # L2 norm
-        'scale': sklearn.preprocessing.scale(data, axis=1), # mean subtracted, divided by standard dev
-    }
-        
-    normed_data = norm_routines[normalizer]
-        
-    traces = make_trialwise(normed_data, trial_lengths)
+    traces = normalize_data(data, norm_method)
     
-    return normed_data, traces
+    # make data trialwise
+    trialwise_data = make_trialwise(traces, trial_lengths) # trials, cells, time
+    
+    # stimtime alignment (either by cell or by trial)
+    new_start *= fr
+    new_start = int(new_start)
+    if stim_times is not None:
+        stim_times = np.array(stim_times)
+        stim_times *= fr
+        trialwise_data = stimalign(trialwise_data, stim_times, new_start)
+    
+    # baseline subtract data for each trial
+    baseline_length = new_start - 1
+    trialwise_data = baseline_subtract(trialwise_data, baseline_length)
+    
+    # optionally cut psths to length
+    if total_length is not None:
+        total_length *= fr
+        total_length = int(total_length)
+        trialwise_data = cut_psths(trialwise_data, length=total_length)
+    
+    return traces, trialwise_data
 
-def do_stimalign(traces, stim_times, align_to):
-    # FIXME: URGENT
-    # ! this isn't correct actually because to stim align by trials you still need a list
-    if isinstance(stim_times, int):
-        traces = stim_align_all_cells(traces, stim_times, align_to)
+def stimalign(trialwise_data: np.ndarray, stim_times: np.ndarray, new_start: int):
+    stim_times = stim_times.round().astype(int)
+    stim_times[np.isnan(stim_times)] = 0 # this is one way to handle cells that don't get stimmed
     
-    elif len(stim_times) > 1:
-        try:
-            assert len(stim_times) == traces.shape[0] # must have same length/size as the number of cells
-            traces = stim_align_by_cell(traces, stim_times, align_to)
+    aligned_data = np.zeros_like(trialwise_data)
+    if len(stim_times) == trialwise_data.shape[1]: # aka number of cells, like stim test
+        for i in range(trialwise_data.shape[0]):
+            aligned_data[i,...] = np.array([np.roll(cell_trace, -amt+new_start) for cell_trace, amt in zip(trialwise_data[i,...], stim_times)])
+    
+    elif len(stim_times) == trialwise_data.shape[0]: # aka number of trials, like ori epoch, although note this breaks in true online mode bc last tiff
+        for i in range(trialwise_data.shape[0]):
+            aligned_data[i,...] = np.roll(trialwise_data[i,...], -int(stim_times[i])+new_start, axis=1)
             
-        except AssertionError:
-            warnings.warn('Length of stim times was greater than one but did not match the number of cells. Stim alignment not done.')
-    return traces
-
-
-def make_traces_from_json(path, *args, **kwargs):
-    """
-    Short cut for loading a json from path and making it into 
-    traces=(trials x cell x time). Passes any args and kwargs to process_data.
-    """
-    data = load_json(path)
-    traces = process_data(data['c'], data['splits'], *args, **kwargs)
-    return traces
+    # TODO: eventually add support for trial x cell stim times (unique each trial)
+    else:
+        warnings.warn('Length of stim times did not match the number of cells or number of trials. Stim alignment not done.')
+            
+    return aligned_data
 
 def make_trialwise(traces, splits):
     """Returns trial x cell x time."""
     traces = np.split(traces, np.cumsum(splits[:-1]).astype(np.uint), axis=1)
     shortest = min([s.shape[1] for s in traces])
     return np.array([a[:, :shortest] for a in traces])
-
-def stim_align_by_cell(traces, times, new_start):
-    """
-    Make stim-aligned PSTHs from trialwise data (eg. trial x cell x time array). The 
-    advantage of doing it this way (trialwise) is the trace for each cell gets rolled around
-    to the other side of the array, thus eliminating the need for nan padding.
-
-    Args:
-        traces (array-like): trial x cell x time array of traces data, typicall from make_trialwise
-        times (array-like): list of stim times for each cell, must match exactly, not sure how it
-                            handles nans yet...
-    """
-    psth = np.zeros_like(traces)
-
-    for i in range(traces.shape[0]):
-        psth[i,:,:] = np.array([np.roll(cell_trace, -amt+new_start) for cell_trace, amt in zip(traces[i,:,:], times)])
-
-    return psth
-
-def stim_align_all_cells(traces, time, new_start):
-    """
-    Make stim-aligned PSTHs from trialwise data (eg. trial x cell x time array). The 
-    advantage of doing it this way (trialwise) is the trace for each cell gets rolled around
-    to the other side of the array, thus eliminating the need for nan padding.
-
-    Args:
-        trialwise_traces (array-like): trial x cell x time array of traces data, typicall from make_trialwise
-        times (array-like): list of stim times for each cell, must match exactly, not sure how it
-                            handles nans yet...
-        new_start (int): frame number where the psths will be aligned to
-    """
-    # FIXME: URGERNT see above, list or single stim time??? depends on how this is working... for a
-    # single trial an int is fine, but for multiple trials you'd want to give a list
-    psth = np.zeros_like(traces)
-
-    for i in range(traces.shape[0]):
-        psth[i,:,:] = np.roll(traces[i,:,:], -int(time[i])+new_start, axis=1)
-
-    return psth
 
 def make_images(caiman_obj):
     Yr, dims, T = cm.load_memmap(caiman_obj.mmap_file)
@@ -143,3 +90,19 @@ def cut_psths(stim_aligned, length=25):
     cut_psths = stim_aligned[:,:,:length]
     return cut_psths
 
+def rolling_baseline_dff(traces):
+    f0s = pd.DataFrame(traces.T).rolling(200, min_periods=1, center=True).quantile(0.2)
+    f0s = f0s.values.T
+    traces = (traces-f0s)/f0s
+    return traces
+
+def normalize_data(data, norm_method):
+    normalize_func = {
+        'none': data, # nothing done...
+        'minmax': lambda x: sklearn.preprocessing.minmax_scale(x, axis=1), # scaled to min max (not abs)
+        'zscore': lambda x: stats.zscore(x, axis=1), # old fashion zscoring
+        'norm': lambda x: sklearn.preprocessing.normalize(x, axis=1), # L2 norm
+        'scale': lambda x: sklearn.preprocessing.scale(x, axis=1), # mean subtracted, divided by standard dev
+    }.get(norm_method)
+        
+    return normalize_func(data)
